e1177d3b6cc5ca4371ab39ad69ced48dd2d00a8a
miaraygithub <139520671+miaraygithub@users.noreply.github.com>
2023-10-04 22:20:02 -0600

    Code done
    NO COMMENTS YET SORRY

--- StringWorkshop.java
+++ StringWorkshop.java
@@ -16,48 +16,67 @@ public class StringWorkshop {
     }
     public static String stringAdd(String baseStr) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter addition: ");
         baseStr += sc.nextLine();
         System.out.println("Current string: " + baseStr);
         return baseStr;
     }
     public static void stringCheck(String baseStr) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter substring: ");
+        String subString = sc.nextLine();
+        if (baseStr.contains(subString)) {
+            System.out.println(subString + " is contained in " + baseStr);
+        } else {
+            System.out.println(subString + " is not contained in " + baseStr);
+        }
     }
 
     public static void stringLen(String baseStr) {
-
+        System.out.println("Lenghth = " + baseStr.length());
     }
 
     public static void stringPre(String baseStr) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter prefix: ");
+        String pre = sc.nextLine();
+        System.out.println("Substring  = " + baseStr.substring(baseStr.indexOf(pre) + pre.length()));
     }
 
     public static void stringPost(String baseStr)  {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter suffix: ");
+        String post = sc.nextLine();
+        System.out.println("Substring  = " + baseStr.substring(0, baseStr.indexOf(post)));
     }
 
     public static void stringMid(String baseStr)  {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter prefix: ");
+        String pre = sc.nextLine();
+        System.out.print("Enter suffix: ");
+        String post = sc.nextLine();
+        System.out.println("Substring = " + baseStr.substring(baseStr.indexOf(pre) + pre.length(), baseStr.indexOf(post)));
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter a starting off string: ");
+        System.out.print("Enter a starting off string: ");
         String baseStr = sc.nextLine();
 
         int option = menu();
         while (option != 7) {
             if (option == 1) {
-                baseStr += stringAdd(baseStr);
+                baseStr = stringAdd(baseStr);
             } else if (option == 2) {
                 stringCheck(baseStr);
             } else if (option == 3) {
                 stringLen(baseStr);
             } else if (option == 4) {
-                stringPost(baseStr);
-            } else if (option == 5) {
                 stringPre(baseStr);
+            } else if (option == 5) {
+                stringPost(baseStr);
             } else if (option == 6) {
                 stringMid(baseStr);
             }
